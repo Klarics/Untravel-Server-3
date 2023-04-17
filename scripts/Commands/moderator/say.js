475bf4aaa7e848158f3b09eb5a3fe84849f2300b
Uriel Antonio <antoniotorres.uri@gmail.com>
2023-04-16 21:24:50 -0600

    command say is working/ not in nametag with spaces

--- scripts/Commands/moderator/say.js
+++ scripts/Commands/moderator/say.js
@@ -1,65 +1,80 @@
-
+import { world } from "@minecraft/server";
 import { sendMsg, sendMsgToPlayer, prefix } from "../../utilsMx.js";
 import { sayOption, sayJust } from "../../custom-ui/sayUi.js";
 
- function sayHelp(player, prefix) {
-//     let commandStatus;
-//     if (!config.customcommands.op) {
-//         commandStatus = "§a[§bDISABLED§a]§r";
-//     }
-//     else {
-//         commandStatus = "§a[§aENABLED§a]§r";
-//     }
-     return sendMsgToPlayer(player, [
-         `\n§b~§aCommand§b~§r: say`,
-//         `§b~§aStatus§b~§r: ${commandStatus}`,
-         `§b~§aUsage§b~§r: say [optional]`,
-         `§b~§aOptional§b~§r: NameTag, Mensaje, help`,
-         `§b~§aDescription§b~§r: Manda mensaje a preguntas a un jugador en su pantalla principal, \n para resivir respuesta la pregunta debe inicair con: §d¿.`,
-         `§b~§aExamples§b~§r:`,
-         `          ${prefix}say ${player.name} Gracias por el sacrificio`,
-         `          ${prefix}say ${player.name} ¿Quieres que te bendiga?`,
-         `          ${prefix}say help`,
-     ]);
- }
-export function say(message, args) {
-    message.cancel = true;
-    let player = message.sender;
-    let argCheck = args[0];
-    let mensaje = args.slice(1).join(" ") || "...";
-    let member;
-    if (!player.hasTag('staffstatus') || !player.hasTag('OlimpO')) {
-        return sendMsgToPlayer(player, `§r§b■§d§lUntravel§eMx§b■§r§c You need to be §eServer-Op§r§c to use this command.`);
-    }
-    if (!args.length) {
-        return sayHelp(player, prefix);
-    }
-	if ((argCheck && args[0].toLowerCase() === "help")) {
-         return sayHelp(player, prefix);
-     }
+function sayHelp(player, prefix) {
+  //     let commandStatus;
+  //     if (!config.customcommands.op) {
+  //         commandStatus = "§a[§bDISABLED§a]§r";
+  //     }
+  //     else {
+  //         commandStatus = "§a[§aENABLED§a]§r";
+  //     }
+  return sendMsgToPlayer(player, [
+    `\n§b~§aCommand§b~§r: say`,
+    //         `§b~§aStatus§b~§r: ${commandStatus}`,
+    `§b~§aUsage§b~§r: say [optional]`,
+    `§b~§aOptional§b~§r: NameTag, Mensaje, help`,
+    `§b~§aDescription§b~§r: Manda mensaje a preguntas a un jugador en su pantalla principal, \n para resivir respuesta la pregunta debe inicair con: §d¿.`,
+    `§b~§aExamples§b~§r:`,
+    `          ${prefix}say ${player.name} Gracias por el sacrificio`,
+    `          ${prefix}say ${player.name} ¿Quieres que te bendiga?`,
+    `          ${prefix}say help`,
+  ]);
+}
+export function say(message, args, args2, commandName) {
+  message.cancel = true;
+  let player = message.sender;
+  let argCheck = args[0];
+  let mensaje = args.slice(1).join(" ") || "                         ...";
+  let member;
+  if (!player.hasTag("staffstatus") || !player.hasTag("OlimpO")) {
+    return sendMsgToPlayer(player, `§cComando desconocido: §r${commandName}§r§c. Revisa que el comando exista y que tengas permiso para usarlo.`);
+  }
+  if (!args.length) {
+    return sayHelp(player, prefix);
+  }
+  if (argCheck && args[0].toLowerCase() === "help") {
+    return sayHelp(player, prefix);
+  }
 
-    for (let pl of world.getPlayers()) {
-        if (pl.nameTag.toLowerCase().includes(args[0].toLowerCase().replace(/"|\\|@/g, ""))) {
-            member = pl;
-        }
+  for (let pl of world.getPlayers()) {
+    if (
+      pl.nameTag
+        .toLowerCase()
+        .includes(args[0].toLowerCase().replace(/"|\\|@/g, ""))
+    ) {
+      member = pl;
     }
-    if (!member) {
-        return sendMsgToPlayer(player, `§r§b■§d§lUntravel§eMx§b■§r§b Jugador no encontrado!`);
-    }
-    if (member === player) {
-        return sendMsgToPlayer(player, `§r§b■§d§lUntravel§eMx§b■§r§b No te puedes mencionar a ti mismo.`);
-    }
+  }
+  if (!member) {
+    return sendMsgToPlayer(
+      player,
+      `§r§b■§d§lUntravel§eMx§b■§r§b Jugador no encontrado!`
+    );
+  }
+  // if (member === player) {
+  //     return sendMsgToPlayer(player, `§r§b■§d§lUntravel§eMx§b■§r§b No te puedes mencionar a ti mismo.`);
+  // }
+  if (
+    player.hasTag("staffstatus") &&
+    player.hasTag("OlimpO") &&
+    mensaje.startsWith("¿")
+  ) {
+    sayOption(player, member, mensaje); // funcion(object,object, string, string)
+    sendMsg(
+      "@a[tag=Adminer]",
+      `§c[!]§r ${player.nameTag}§a preguntó: §r${mensaje} §aa §r${member.nameTag}.`
+    );
 
-    if ((player.hasTag('staffstatus') && player.hasTag('OlimpO')) && mensaje.startsWith("¿")) {
-        sendMsg("@a[tag=Adminer]", `§r§b■§d§lUntravel§eMx§b■§r ${player.nameTag}§a preguntó: §r${mensaje} §aa §r${member}  .`);
-        sayOption(player, member, args)
-
-    //    player.runCommandAsync(`playsound minecraft:xp_levelUp ${player}  `)
-        return;
-    }
-    else if (player.hasTag('staffstatus') && player.hasTag('OlimpO')) {
-        sendMsg("@a[tag=Adminer]", `§r§b■§d§lUntravel§eMx§b■§r ${player.nameTag}§a dijo: §r${mensaje} §aa §r${member}  .`);
-        sayJust(player, member, args)
-        return;
-    }
+    //
+    return;
+  } else if (player.hasTag("staffstatus") && player.hasTag("OlimpO")) {
+    sayJust(player, member, mensaje);
+    sendMsg(
+      "@a[tag=Adminer]",
+      `§c[!]§r ${player.nameTag}§a dijo: §r${mensaje} §aa §r${member.nameTag}.`
+    );
+    return;
+  }
 }
--- scripts/custom-ui/sayUi.js
+++ scripts/custom-ui/sayUi.js
@@ -1,29 +1,28 @@
-import { ActionFormData } from "@minecraft/server-ui";
+import { ActionFormData, MessageFormData } from "@minecraft/server-ui";
 import { sendMsgToPlayer } from "../utilsMx";
 
-export function sayOption(playerid, member, args) {
-  const gui = new ActionFormData();
-  // ★━━━━━━━━━━━━━━━━━━━━★
-  gui.title("");
-  gui.body(
-    `\n <${playerid}> pregunta: \n ${args}`
+export function sayOption(playerid, member, args) { // funcion(object,object, string, string)
+  const dus = new ActionFormData();
+  dus.title(`★━━━━━━━━<§6${playerid.nameTag}§r>━━━━━━━━★`);
+  dus.body(
+    `\n\n${args}\n\n\n\n`
   );
-  gui.button("§bOk");
-  gui.button("§cNo")
-  gui.show(member).then((result) => {
+  dus.button("§bSí");
+  dus.button("§cNo")
+  dus.show(member).then((result) => {
     if (result.canceled) {
-      sendMsgToPlayer(member, `§eNo puedes ignorar me para siempre.`)
-      sendMsgToPlayer(playerid, `§r§b■§d§lUntravel§eMx§b■§r ${member} §bignoró tu pregunta .`)
+      sendMsgToPlayer(member, `§aIgnoraste a §b${member.nameTag}.`)
+      sendMsgToPlayer(playerid, `§r§b■§d§lUntravel§eMx§b■§r ${member.nameTag} §bignoró tu pregunta .`)
     }
 
 
     if (result.selection === 0) {
-      sendMsgToPlayer(playerid, `§r§b■§d§lUntravel§eMx§b■§r ${member} §bAceptó .`)
+      sendMsgToPlayer(playerid, `§r§b■§d§lUntravel§eMx§b■§r ${member.nameTag} §bRespondió §eSí.`)
       sendMsgToPlayer(member, `§r§b■§d§lUntravel§eMx§b■§r §aTu respuesta fue enviada .`)
 
     }
     if (result.selection === 1) {
-      sendMsgToPlayer(playerid, `§r§b■§d§lUntravel§eMx§b■§r ${member} §bLo negó .`)
+      sendMsgToPlayer(playerid, `§r§b■§d§lUntravel§eMx§b■§r ${member.nameTag} §brespondió §eNo.`)
       sendMsgToPlayer(member, `§r§b■§d§lUntravel§eMx§b■§r §aTu respuesta fue enviada .`)
     }
   });
@@ -31,15 +30,18 @@ export function sayOption(playerid, member, args) {
 
 
 export function sayJust(playerid, member, args) {
-  const gui = new ActionFormData();
-  // ★━━━━━━━━━━━━━━━━━━━━★
-  gui.title("");
-  gui.body(
-    `\n <${playerid}>: \n ${args}`
+  const fus = new ActionFormData();
+  fus.title(`★━━━━━━━━<§6${playerid.nameTag}§r>━━━━━━━━★`);
+  fus.body(
+    `\n\n${args}\n\n\n\n`
   );
-  gui.show(member).then((result) => {
+  fus.button("...");
+  fus.show(member).then((result) => {
     if (result.canceled) {
-      sendMsgToPlayer(playerid, `§r§b■§d§lUntravel§eMx§b■§r ${member} §bya vio tu mensaje .`)
+      sendMsgToPlayer(playerid, `§r§b■§d§lUntravel§eMx§b■§r ${member.nameTag} §bTalvez vio tu mensaje .`)
     }
+     if (result.selection === 0) {
+       sendMsgToPlayer(playerid, `§r§b■§d§lUntravel§eMx§b■§r ${member.nameTag} §bYa vio tu mensaje .`)
+     }
   });
 }
