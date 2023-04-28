d6dfdc58bae1e4c44cdc08ca72ae0f32c01fa7f1
Uriel Antonio <antoniotorres.uri@gmail.com>
2023-04-28 00:16:40 -0600

    added aworkin general class confirmation battle

---
+++ scripts/custom-ui/comfirmUi.js
@@ -0,0 +1,43 @@
+import { ActionFormData } from "@minecraft/server-ui";
+import { paladinClass } from "./paladinUi";
+
+const functionDefinitions = Object.setPrototypeOf(
+  {
+    paladinClass: paladinClass,
+  },
+  null
+);
+
+export function comfirmBattle(playerId, clase) {
+  let functionName = clase
+  let confirmUi = new ActionFormData();
+  confirmUi.title("§4A la Batalla");
+  confirmUi.body(
+    "\n§6Si das continuar y no has selecionado tus habiliades disponibles podras estar en desventaja"
+  );
+  confirmUi.button("§4Continuar");
+  confirmUi.button("Regresar");
+  confirmUi.show(playerId).then((result) => {
+     if (result.canceled) {
+        if (!(functionName in functionDefinitions)) {
+            playerId.runCommandAsync(
+              `say §cFuncion desconocido: §r§l${functionName}§r§c. Revisa que la funcion este registrada o avisa a un admin con una captura.`
+            );
+          }
+          functionDefinitions[functionName](playerId);
+       }
+    if (result.selection == 0) {
+      playerId.runCommandAsync(
+        `tellraw @a {"rawtext":[{"text":"§aEn batalla. Espera 5 minutos para cambiar habilidades"}]}`
+      );
+    }
+    if (result.selection == 1) {
+      if (!(functionName in functionDefinitions)) {
+        playerId.runCommandAsync(
+          `say §cFuncion desconocido: §r§l${functionName}§r§c. Revisa que la funcion este registrada o avisa a un admin con una captura.`
+        );
+      }
+      functionDefinitions[functionName](playerId);
+    }
+  });
+}
